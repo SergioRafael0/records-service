a32ab84e8d51ff0625ab1aba8c718e089974d761
SergioRafael0 <se.delcampo@duocuc.cl>
2026-05-06 22:27:21 -0400

    first commit

---
+++ records-service/src/test/java/cl/colegio/records_service/RecordsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.colegio.records_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
