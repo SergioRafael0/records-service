9e9b901af6c00a5ba317c2f43ae01742c52b05a6
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2026-07-01 03:18:22 -0400

    fix(webclient): add LoadBalanced annotation to WebClient builder for improved load balancing

--- records-service/src/main/java/cl/colegio/records_service/RecordsServiceApplication.java
+++ records-service/src/main/java/cl/colegio/records_service/RecordsServiceApplication.java
@@ -3,6 +3,7 @@ package cl.colegio.records_service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,10 +15,14 @@ public class RecordsServiceApplication {
 	}
 
 	@Bean
-	public WebClient webClient(@Value("${api.user-service.base-url}") String baseUrl) {
-		return WebClient.builder()
-				.baseUrl(baseUrl)
-				.build();
+	@LoadBalanced
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
+	}
+
+	@Bean
+	public WebClient webClient(WebClient.Builder builder, @Value("${api.user-service.base-url}") String baseUrl) {
+		return builder.baseUrl(baseUrl).build();
 	}
 
 }
